d6191fdf15d85368fda16339724940c34a72dc0b
Guillaume Turchini <guillaume.turchini@gmail.com>
2018-10-31 09:32:52 +0100

    Adding debug env var to show debug output on stderr

--- 4-miniGrep/minigrep_lib/src/config/mod.rs
+++ 4-miniGrep/minigrep_lib/src/config/mod.rs
@@ -1,6 +1,9 @@
+use std::env;
+
 pub struct Config<'a> {
     text: &'a str,
     file: &'a str,
+    debug: bool
 }
 
 impl<'a> Config<'a> {
@@ -10,22 +13,40 @@ impl<'a> Config<'a> {
     pub fn file(&self) -> &str {
         self.file
     }
+    pub fn debug(&self) -> bool {
+        self.debug
+    }
 }
 
 pub fn parse_args<'a>(args: &'a Vec<String>) -> Result<Config<'a>, String> {
-    //eprintln!("Called with : {:?}", args);
+    let debug = is_debug_enabled();
+
+    if debug {
+        eprintln!("Called with : {:?}", args);
+    }
 
     if args.len() != 3 {
         show_usage(&args[0]);
         return Err(format!("Wrong number of arguments : expected 3, got 2"));
     }
 
+    let text = &args[1];
+    let file = &args[2];
+
     return Ok(Config {
-        text: &args[1],
-        file: &args[2],
+        text,
+        file,
+        debug,
     });
 }
 
 fn show_usage(executable_name: &str) {
     eprintln!("Usage : {} <str_to_search> <file>", executable_name);
+}
+
+fn is_debug_enabled() -> bool {
+    match env::var("DEBUG") {
+        Ok(v) => v.eq("true") || v.eq("y"),
+        Err(_) => false
+    }
 }
--- 4-miniGrep/minigrep_lib/src/searcher/mod.rs
+++ 4-miniGrep/minigrep_lib/src/searcher/mod.rs
@@ -2,19 +2,34 @@ use std::fs;
 use ::config::Config;
 
 pub fn search(conf: Config) {
-    eprintln!("Looking for \"{}\" in file \"{}\"", conf.text(), conf.file());
+    if conf.debug() {
+        eprintln!("Looking for \"{}\" in file \"{}\"", conf.text(), conf.file());
+    }
 
     let content = fs::read_to_string(conf.file()).expect("Cannot read file");
     //eprintln!("File content :\n\n{}", content);
 
-    search_in_string(conf.text(), content);
+    let matching_lines = search_in_string(conf.text(), &content);
+
+    if conf.debug() {
+        eprintln!("Got {} results", matching_lines.len());
+    }
+
+    for line in matching_lines {
+        println!("{}", line);
+    }
 }
 
-fn search_in_string(text_to_search: &str, file_content: String) {
+/// Returns all lines containing the text to search in the string
+fn search_in_string<'a>(text_to_search: &str, file_content: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&'a str> = Vec::new();
+
     file_content.lines()
         .for_each(|line| {
             if line.contains(text_to_search) {
-                println!("{}", line);
+                result.push(line);
             }
         });
+
+    return result;
 }
